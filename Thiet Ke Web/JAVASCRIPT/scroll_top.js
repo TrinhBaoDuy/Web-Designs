22b3b2a186c52d7fa18c65ef7a5fefdf7621d69a
TrinhBaoDuy <2051050075duy@ou.edu.vn>
2023-08-16 14:01:36 +0700

    FIX

--- Thiet Ke Web/JAVASCRIPT/scroll_top.js
+++ Thiet Ke Web/JAVASCRIPT/scroll_top.js
@@ -1,30 +1,37 @@
 $(document).ready(function() {
-	$("#top").hide()
-	$(window).scroll(function() {
-		if ($(this).scrollTop() >= 550)
-			$("#top").show("slow")
-		else {
-			$("#top").hide("slow")
-		}
-	})
+	// $("#top").hide()
+	// $(window).scroll(function() {
+	// 	if ($(this).scrollTop() >= 550)
+	// 		$("#top").show("slow")
+	// 	else {
+	// 		$("#top").hide("slow")
+	// 	}
+	// })
 
-	$("#top").click(function() {
-		$("html, body").animate({
-			scrollTop: 0
-		}, 1000)
-	})
+	// $("#top").click(function() {
+	// 	$("html, body").animate({
+	// 		scrollTop: 0
+	// 	}, 1000)
+	// })
 
 	// Open/Close Sub Nav Menu
-	$(".nav-mobile-btn").click(function() {
-		$(".sub-nav").addClass(".open-nav");
-		// $(".sub-nav.open-nav").show();
-	})
+	// $(".nav-mobile-btn").click(function() {
+	// 	$(".sub-nav").addClass(".open-nav");
+	// 	$(".sub-nav.open-nav").show();
+	// })
 
-	// function showSubNav() {
-	// 	let modal = document.querySelector(".sub-nav");
-	// 	modal.classList.add("open-nav");
-	// }
+	
 
 	// btnMenu = document.querySelector(".nav-mobile-btn");
 	// btnMenu.addEventListener("click", showSubNav);
 })
+
+function showSubNav() {
+	var spanElement = document.querySelector(".sub-nav");
+	spanElement.classList.add("open-nav");
+}
+
+function hideSubNav() {
+	var spanElement = document.querySelector(".sub-nav");
+	spanElement.classList.remove("open-nav");
+}
