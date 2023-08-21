3806f4bac9a74cb04493e61b044452fc289b2ce3
Tuan Dang <dstuan02@gmail.com>
2023-08-21 21:59:47 +0700

    Fixed btn bug & completed project

--- Thiet Ke Web/JAVASCRIPT/program.js
+++
@@ -1,9 +0,0 @@
-(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 500) {
-      $(".btn-primary").addClass("show");
-    } else {
-      $(".btn-primary").removeClass("show");
-    }
-  });
-});
